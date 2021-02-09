e5d92776a53c6a0ba9d8935e0390240679c848c0
swagatika panda <swagatika20panda@gmail.com>
2021-02-09 16:38:15 +0530

    first commit

---
+++ src/signup.js
@@ -0,0 +1,12 @@
+import React ,{Component} from  "react";
+
+
+class Signup extends Components{
+    render(){
+        return(
+            <div>Signup</div>
+        )
+    }
+};
+export default Signup;
+
